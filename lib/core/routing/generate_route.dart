dd7faf84e9c4e9bc1b9d10774d92d638125f7766
mohammedkhalil <klilmohammed9@gmail.com>
2025-05-08 18:10:56 +0300

    feat: finish login screen

--- lib/core/routing/generate_route.dart
+++ lib/core/routing/generate_route.dart
@@ -2,7 +2,6 @@ import 'package:flower_tracking_app/core/routing/defined_routes.dart';
 import 'package:flower_tracking_app/modules/authentication/domain/entities/logged_driver_data/logged_driver_data_response_entity.dart';
 import 'package:flower_tracking_app/modules/onboarding/ui/onboarding_screen.dart';
 import 'package:flutter/material.dart';
-
 import '../../modules/authentication/ui/login/login_screen.dart';
 import '../../modules/home/ui/home_screen.dart';
 
