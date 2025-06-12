29ef3dfdfecb34f0dfa4dc92abfd10d703715386
Mohamed Samir <mohamed.samir.ibrahim98@gmail.com>
2025-06-13 02:19:44 +0300

    fix: some fixes in code

--- lib/modules/home/data/models/orders_client_model.g.dart
+++ lib/modules/home/data/models/orders_client_model.g.dart
@@ -49,7 +49,7 @@ Map<String, dynamic> _$MetadataToJson(Metadata instance) => <String, dynamic>{
 // ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
 
 class _OrdersApiClient implements OrdersApiClient {
-  _OrdersApiClient(this._dio, {this.baseUrl}) {
+  _OrdersApiClient(this._dio,) {
     baseUrl ??= 'https://flower.elevateegy.com/';
   }
 
