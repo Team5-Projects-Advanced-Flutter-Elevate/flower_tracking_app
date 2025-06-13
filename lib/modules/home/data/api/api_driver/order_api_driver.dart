1770ae139c91325cd2e966357d4a4b02fe8040fc
Mohamed Samir <mohamed.samir.ibrahim98@gmail.com>
2025-06-13 12:45:58 +0300

    fix: fix in code

--- lib/modules/home/data/api/api_driver/order_api_driver.dart
+++ lib/modules/home/data/api/api_driver/order_api_driver.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:flower_tracking_app/modules/home/data/models/driver/pending_orders_model.dart'
-    as orders_api;
+    as ordersApi;
 
 @module
 abstract class OrdersApiDriverProvider {
   @lazySingleton
-  orders_api.OrdersRemoteDataSource providerApiClient(Dio dio) {
-    return orders_api.OrdersRemoteDataSource(dio);
+  ordersApi.OrdersRemoteDataSource providerApiClient(Dio dio) {
+    return ordersApi.OrdersRemoteDataSource(dio);
   }
 }
