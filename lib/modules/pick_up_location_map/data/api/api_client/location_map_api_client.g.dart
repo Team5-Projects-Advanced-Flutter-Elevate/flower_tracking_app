e16e7213a59db4ac4bccf62fa240eb21fbd56efd
mk369 <klilmohammed9@gmail.com>
2025-06-14 10:04:12 +0300

    feat: finish solving conflicts of develop branch

--- lib/modules/pick_up_location_map/data/api/api_client/location_map_api_client.g.dart
+++ lib/modules/pick_up_location_map/data/api/api_client/location_map_api_client.g.dart
@@ -9,7 +9,7 @@ part of 'location_map_api_client.dart';
 // ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
 
 class _LocationMapApiClient implements LocationMapApiClient {
-  _LocationMapApiClient(this._dio, {this.baseUrl}) {
+  _LocationMapApiClient(this._dio, {this.baseUrl}){
     errorLogger = null;
   }
 
