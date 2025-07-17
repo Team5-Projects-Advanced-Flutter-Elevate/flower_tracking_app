bd2ecd6388e9719993b12be08a6ecb71e99531e6
mk369 <klilmohammed9@gmail.com>
2025-07-17 20:41:44 +0300

    feat: make the marker changes when the current location do

--- lib/modules/pick_up_location_map/ui/view_model/location_map_view_model.dart
+++ lib/modules/pick_up_location_map/ui/view_model/location_map_view_model.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'dart:nativewrappers/_internal/vm/lib/async_patch.dart';
 import 'package:flower_tracking_app/core/apis/api_result/api_result.dart';
 import 'package:flower_tracking_app/core/utilities/bloc_observer/location_permission_denied.dart';
 import 'package:flower_tracking_app/modules/pick_up_location_map/domain/entities/directions_request/directions_request_entity.dart';
